171fa2c92b886fa9e62f57bb4bcd40b80460f611
DroobingNoob <98704883+DroobingNoob@users.noreply.github.com>
2025-07-29 09:28:33 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -222,7 +222,7 @@ const Header: React.FC<HeaderProps> = ({
                 {/* Mobile Logo - Centered and Prominent */}
                <div className="flex justify-center w-full relative">
   <img 
-    src="/1000156095.jpg"
+     src="https://res.cloudinary.com/dcodirzsc/image/upload/v1753761357/gaming_community_eykswy.jpg"
     alt="Gaming Community"
    className="w-16 h-16 xs:w-18 xs:h-18 sm:w-20 sm:h-20 object-contain hover:scale-105 transition-transform duration-300 shadow-lg rounded-lg cursor-pointer relative left-2 sm:left-4 md:left-0"
     onClick={() => navigate('/')}
