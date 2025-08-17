664c8720aec6323531ac2c8ee9b21ee31b5f1b4a
DroobingNoob <98704883+DroobingNoob@users.noreply.github.com>
2025-08-17 18:30:09 +0530

    Updated GamesPage.tsx

--- src/pages/GamesPage.tsx
+++ src/pages/GamesPage.tsx
@@ -34,6 +34,7 @@ const GamesPage: React.FC = () => {
   };
 
   const handleGameClick = (game: Game) => {
+    sessionStorage.setItem("gamesPage", currentPage.toString()); 
     navigate(`/games/${game.id}`);
   };
 
