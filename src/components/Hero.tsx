505d6b31a7b38892b67449f9ba95bbeb2da9d540
DroobingNoob <98704883+DroobingNoob@users.noreply.github.com>
2025-09-14 14:50:37 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,6 +29,10 @@ const goToPrevSlide = () => {
   setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
 };
 
+const goToAllGames = () => {
+ navigate('/games');
+};
+
 
   // Auto-slide every 5 seconds
   useEffect(() => {
@@ -71,7 +75,7 @@ const goToPrevSlide = () => {
       image: "https://images.pexels.com/photos/194511/pexels-photo-194511.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080",
       overlay: "bg-gradient-to-br from-orange-900/95 via-red-900/90 to-orange-900/85",
       buttonText: "Browse Games",
-      buttonAction: navigate('/games'),
+      buttonAction: goToAllGames,
       isPromo: false
     }
   ];
