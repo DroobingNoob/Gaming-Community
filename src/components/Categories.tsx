44f62cf6d00551d58816df93504322290294a795
DroobingNoob <98704883+DroobingNoob@users.noreply.github.com>
2025-09-14 14:54:40 +0530

    Updated Categories.tsx

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -15,7 +15,7 @@ const Categories: React.FC<CategoriesProps> = ({ onViewAllGames, onViewSubscript
       id: 1,
       name: "PlayStation Games",
       icon: <PlayCircle className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
-      description: "PS4 & PS5 exclusive titles",
+      description: "Click to Browse PS4 & PS5 exclusive titles",
       image: "https://images.pexels.com/photos/1298601/pexels-photo-1298601.jpeg?auto=compress&cs=tinysrgb&w=400&h=300",
       gameCount: "300+",
       onClick: () => navigate('/games')
@@ -24,7 +24,7 @@ const Categories: React.FC<CategoriesProps> = ({ onViewAllGames, onViewSubscript
       id: 2,
       name: "Subscriptions",
       icon: <Crown className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
-      description: "Game Pass & PS Plus",
+      description: "Click to Browse Subscriptions"",
       image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&w=400&h=300",
       gameCount: "10+",
       onClick: () => navigate('/subscriptions')
