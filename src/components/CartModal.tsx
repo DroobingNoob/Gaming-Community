94fcf62d09d90485339c8efce9a684093245bee8
DroobingNoob <98704883+DroobingNoob@users.noreply.github.com>
2025-10-21 21:36:55 +0530

    Updated CartModal.tsx

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -150,9 +150,9 @@ const CartModal: React.FC<CartModalProps> = ({
                 {/* Pro Tip Banner */}
               {total < 1299 && (
                 <div className="bg-gradient-to-r from-cyan-100 to-blue-100 border border-cyan-300 text-cyan-900 rounded-xl p-3 sm:p-4 shadow-md text-sm sm:text-base font-medium text-center">
-                  💡 Pro Tip: Add products worth ₹{(1299 - total).toFixed(0)} more and get 
-                  <span className="font-bold text-cyan-700"> ₹100 OFF </span> 
-                  with coupon <span className="font-bold">PLAY100</span>!
+                  💡 Pro Tip: Add products worth ₹{(799 - total).toFixed(0)} more and get 
+                  <span className="font-bold text-cyan-700"> 10% OFF UPTO ₹150</span> 
+                  with coupon <span className="font-bold">FESTIVALOFF</span>!
                 </div>
               )}
               {total >= 1299 && total < 3000 && (
